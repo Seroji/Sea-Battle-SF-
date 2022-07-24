02d9df619dbdfd24ef631e13aa60d63365e6276f
Seroji <gamexr6@mail.ru>
2022-07-24 17:41:24 +0300

    Class Dot was created

--- SeaBattle.py
+++ SeaBattle.py
@@ -16,3 +16,15 @@ class BoardShipWrongException(BoardException):
     pass
 
 
+class Dot:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return f"Dot({self.x}, {self.y})"
+
+
